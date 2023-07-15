f238bad6bd7600955b258186e2812e5f847c4a8e
Trần Huy Hoàng <wujor01@gmail.com>
2023-07-15 09:36:14 +0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -9,7 +9,7 @@ document.addEventListener("dblclick", function (event) {
             // Create a popover element
             var popover = document.createElement("div");
             popover.className = "my-popover";
-            popover.textContent = date.toISOString();
+            popover.textContent = convertDateToString(date);
             document.body.appendChild(popover);
 
             // Position the popover near the selected text
@@ -28,3 +28,24 @@ document.addEventListener("dblclick", function (event) {
     }
 
 });
+
+
+function convertDateToString(date) {
+    // Extract date components
+    var year = date.getFullYear();
+    var month = String(date.getMonth() + 1).padStart(2, '0');
+    var day = String(date.getDate()).padStart(2, '0');
+
+    // Extract time components
+    var hours = String(date.getHours()).padStart(2, '0');
+    var minutes = String(date.getMinutes()).padStart(2, '0');
+    var seconds = String(date.getSeconds()).padStart(2, '0');
+
+    // Get the timezone offset in minutes and convert to hours
+    var timezoneOffsetHours = Math.floor(date.getTimezoneOffset() / 60);
+
+    // Construct the formatted string
+    var formattedDate = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds + ' GMT' + (timezoneOffsetHours < 0 ? '+' : '') + Math.abs(timezoneOffsetHours);
+
+    return formattedDate;
+}
